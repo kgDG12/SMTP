c06166fe2ff2b4eed649706f3863cb2781e58754
DannyGlade <dg@kg.com>
2022-04-01 11:56:56 +0530

    base

---
+++ send.php
@@ -0,0 +1,36 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/phpmailer/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+
+$mail = new PHPMailer();
+$mail->isSMTP();
+$mail->Mailer = "smtp";
+
+$mail->SMTPDebug  = 1;  
+$mail->SMTPAuth   = TRUE;
+$mail->SMTPSecure = "tls";
+$mail->Port       = 2525;
+$mail->Host       = "smtp.mailtrap.io";
+$mail->Username   = "fbf952da3faa0e";
+$mail->Password   = "d81054b0a18b6e";
+
+$mail->IsHTML(true);
+$mail->AddAddress("darshan69420@yobro.com", "Darshan");
+$mail->SetFrom("ed14a28336-06efd7@inbox.mailtrap.io", "KG Darshan");
+// $mail->AddReplyTo("reply-to-email@domain", "reply-to-name");
+// $mail->AddCC("cc-recipient-email@domain", "cc-recipient-name");
+$mail->Subject = "This is Test 69";
+$content = "<b>This is a Test Email sent via Gmail SMTP Server using PHP mailer class.</b>";
+
+$mail->MsgHTML($content); 
+if(!$mail->Send()) {
+  echo "Error while sending Email.";
+  var_dump($mail);
+} else {
+  echo "Email sent successfully";
+}
